d62d55250f1ddab8fd3280055c328dfa197b3047
Smith2008S <jhoinerrojas@ibm.com>
2020-03-11 17:42:23 -0500

    v1c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ListaComponent } from './lista/lista.component';
@@ -10,7 +12,9 @@ import { ListaComponent } from './lista/lista.component';
     ListaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.css']
 })
-export class ListaComponent implements OnInit {
 
-  constructor() { }
+export class ListaComponent {
+  item: string;
+  items = [];
 
-  ngOnInit(): void {
+  onClick() {
+    if (this.item.trim().length)
+    {
+      this.items.push({ name: this.item });
+      this.item = '';
+    }      
   }
 
 }
+
+
